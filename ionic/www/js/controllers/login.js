a9600baf5eb87170a20fb621021ba17bdd361224
Luiz Fernando Sanches <luiz.sanches8910@gmail.com>
2016-08-17 23:43:39 -0300

    Botão Voltar e Botão Voltar Hardware

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,10 +1,21 @@
-angular.module('starter.controllers')
-    .controller('LoginCtrl', ['$scope', 'OAuth', '$state', '$login', function ($scope, OAuth, $state, $login) {
+angular.module('starter.controllers', ['ionic'])
+    .controller('LoginCtrl', ['$scope', 'OAuth', '$state', '$login', '$ionicPlatform', '$ionicHistory',
+        function ($scope, OAuth, $state, $login, $ionicPlatform, $ionicHistory) {
 
         $scope.user = {
             username: 'client@buscasorocaba.com.br',
             password: 'rbts@2016'
         };
+
+            $ionicPlatform.registerBackButtonAction(function(e) {
+                if($state.current.name == 'dashboard'){
+                    ionic.Platform.exitApp();
+                }
+                else{
+                    $ionicHistory.goBack();
+                }
+
+            }, 101);
 
         $scope.login = function () {
             OAuth.getAccessToken($scope.user)
